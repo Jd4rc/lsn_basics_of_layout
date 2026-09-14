88f6b2df6f029fc2fb212d07b349136a772ba792
dosed <e.nmitenin@gmail.com>
2026-09-14 11:08:17 +0300

    feat: register Category in admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,15 +1,14 @@
-# Register your models here.
-
-
 from django.contrib import admin
 from .models import Category, Product
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'stock', 'is_active')
+    list_display = ('name', 'category', 'price', 'stock', 'is_active', 'condition')
     list_filter = ('category', 'is_active')
     search_fields = ('name',)
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
